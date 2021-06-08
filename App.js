30ea9c8cdfd9391248ca00737670c02154653603
YJ Soon <yjsoon@tinkertanker.com>
2021-06-08 16:52:58 +0800

    Set up the add color function

--- App.js
+++ App.js
@@ -1,16 +1,41 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { FlatList, StyleSheet, Text, View, Button } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import BlockRGB from "./components/BlockRGB";
 
 function HomeScreen() {
+  const [colors, setColors] = useState([
+    { red: 255, green: 0, blue: 0, id: "0" },
+    { red: 0, green: 255, blue: 0, id: "1" },
+    { red: 0, green: 0, blue: 255, id: "2" },
+  ]);
+
+  function renderItem({ item }) {
+    return <BlockRGB red={item.red} green={item.green} blue={item.blue} />;
+  }
+
+  function addColor() {
+    setColors([
+      ...colors,
+      {
+        red: Math.floor(Math.random() * 255),
+        green: Math.floor(Math.random() * 255),
+        blue: Math.floor(Math.random() * 255),
+        id: `${colors.length}`,
+      },
+    ]);
+  }
+
   return (
     <View style={styles.container}>
-      <BlockRGB red={255} green={0} blue={0} />
-      <BlockRGB red={0} green={255} blue={0} />
-      <BlockRGB red={0} green={0} blue={255} />
+      <Button onPress={addColor} title="Add Color" />
+      <FlatList
+        style={{ width: "100%" }}
+        data={colors}
+        renderItem={renderItem}
+      />
     </View>
   );
 }
