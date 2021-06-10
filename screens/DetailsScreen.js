1555a81653150978308af648ec89ef36b3a1a9f3
YJ Soon <yjsoon@tinkertanker.com>
2021-06-10 14:57:17 +0800

    Some styling for details screen

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -1,13 +1,29 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function DetailsScreen({ route }) {
   const { red, green, blue } = route.params;
+  const avgColor = (red + green + blue) / 3;
+  const textColor = avgColor < 140 ? "white" : "black";
+
   return (
-    <View>
-      <Text>Red: {red}</Text>
-      <Text>Green: {green}</Text>
-      <Text>Blue: {blue}</Text>
+    <View
+      style={[
+        styles.container,
+        { backgroundColor: `rgba(${red}, ${green}, ${blue}, 1)` },
+      ]}
+    >
+      <Text style={{ color: textColor }}>Red: {red}</Text>
+      <Text style={{ color: textColor }}>Green: {green}</Text>
+      <Text style={{ color: textColor }}>Blue: {blue}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
